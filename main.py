6a4bdbb9565210b6d4220babff5cc52440ddd168
ccszbd <56284775+ccszbd@users.noreply.github.com>
2022-06-02 22:52:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+
+def read_csv(filename):
+    with open(filename,'r') as file:
+        cf = csv.reader(file)
+        for r in cf:
+            sub = r[2]
+            obj = r[5]
+            p = float(r[6])
+            rel = r[7]
+            if p<0.8:
+                print(sub,obj,rel)
+            else:
+                print("wrong_main_2232")
+if __name__ == '__main__':
+    read_csv('bacteria_bd_disease_1.csv')
+
